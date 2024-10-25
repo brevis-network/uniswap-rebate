99b7047a6862eeb31846f7452f16073929dcc8a2
Junda Liu <junda@celer.network>
2024-10-25 16:30:04 -0700

    same interface for both single and batch proofs

--- contracts/src/IBrevisProof.sol
+++ contracts/src/IBrevisProof.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-
+import "./Lib.sol";
 interface IBrevisProof {
     function submitProof(
         uint64 _chainId,
         bytes calldata _proofWithPubInputs
     ) external returns (bytes32 requestId, bytes32 appCommitHash, bytes32 appVkHash);
+    
+    function submitAggProof(
+        uint64 _chainId,
+        bytes32[] calldata _requestIds,
+        bytes calldata _proofWithPubInputs
+    ) external;
+
+    function validateAggProofData(uint64 _chainId, Brevis.ProofData[] calldata _proofDataArray) external view;
 }
--- contracts/src/ZkRebate2.sol
+++ contracts/src/ZkRebate2.sol
@@ -5,6 +5,7 @@ import {PoolId} from "v4-core/types/PoolId.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
+import {Brevis} from "./Lib.sol";
 import {IBrevisProof} from "./IBrevisProof.sol";
 
 contract ZkRebate2 {
@@ -35,40 +36,78 @@ contract ZkRebate2 {
     // 2. check msg.sender matches output[0:20]
     // 3. parse output and sum amount
     // 4. uni.transfer
+
+    // same func for signle and aggregated proof. for single proof, _proofIds and _proofDataArray are empty
     function claimWithZkProof(
         address receiver, // uni will be sent to this address
         bytes calldata _proof,
-        bytes calldata _appOutput
+        bytes[] calldata _appCircuitOutputs,
+        bytes32[] calldata _proofIds,
+        Brevis.ProofData[] calldata _proofDataArray
     ) external {
-        // check proof
-        (, bytes32 appCommitHash, bytes32 appVkHash) = brvProof.submitProof(uint64(block.chainid), _proof);
-        require(appVkHash == vkHash, "mismatch vkhash");
-        require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
-        require(_appOutput.length >= 84, "not enough app output");
-        require((_appOutput.length-20) % 64 == 0, "incorrect app output");
-    
-        // sender is msg.sender for Swap
-        address sender = address(bytes20(_appOutput[0:20]));
-        require(msg.sender==sender, "mismatch msg.sender and circuit output");
-
         uint256 amount = 0; // total uni
-        for (uint256 idx=20;idx<_appOutput.length;idx+=64) {
-            bytes32 poolid = bytes32(_appOutput[idx:idx+32]);
-            if(poolid == 0) {
-                break; // circuit may have zero fillings due to fixed length, ends loop early to save gas
+        if (_appCircuitOutputs.length == 1 ) {
+            bytes calldata _appOutput = _appCircuitOutputs[0];
+            // check proof
+            (, bytes32 appCommitHash, bytes32 appVkHash) = brvProof.submitProof(uint64(block.chainid), _proof);
+            require(appVkHash == vkHash, "mismatch vkhash");
+            require(appCommitHash == keccak256(_appOutput), "invalid circuit output");
+            require(_appOutput.length >= 84, "not enough app output");
+            require((_appOutput.length-20) % 64 == 0, "incorrect app output");
+        
+            // sender is msg.sender for Swap
+            address sender = address(bytes20(_appOutput[0:20]));
+            require(msg.sender==sender, "mismatch msg.sender and circuit output");
+
+            for (uint256 idx=20;idx<_appOutput.length;idx+=64) {
+                bytes32 poolid = bytes32(_appOutput[idx:idx+32]);
+                if(poolid == 0) {
+                    break; // circuit may have zero fillings due to fixed length, ends loop early to save gas
+                }
+                if(poolId[poolid]) {
+                    uint64 beginBlk = uint64(bytes8(_appOutput[idx+32:idx+40]));
+                    uint64 endBlk = uint64(bytes8(_appOutput[idx+40:idx+48]));
+                    if(beginBlk>lastBlockNum[sender][poolid]) {
+                        lastBlockNum[sender][poolid] = endBlk;
+                        amount += uint128(bytes16(_appOutput[68:84]));
+                    }
+                }
             }
-            if(poolId[poolid]) {
-                uint64 beginBlk = uint64(bytes8(_appOutput[idx+32:idx+40]));
-                uint64 endBlk = uint64(bytes8(_appOutput[idx+40:idx+48]));
-                if(beginBlk>lastBlockNum[sender][poolid]) {
-                    lastBlockNum[sender][poolid] = endBlk;
-                    amount += uint128(bytes16(_appOutput[68:84]));
+            // emit event?
+            if(amount > 0 ) {
+                uni.transfer(receiver, amount);
+            }
+            return;
+        }
+        // batch mode
+        brvProof.submitAggProof(uint64(block.chainid), _proofIds, _proof);
+        brvProof.validateAggProofData(uint64(block.chainid), _proofDataArray);
+        // verify data and output
+        for (uint256 i=0;i<_proofIds.length;i++) {
+            require(_proofDataArray[i].appVkHash == vkHash, "mismatch vkhash");
+            require(_proofDataArray[i].commitHash == _proofIds[i], "invalid proofId");
+            require(_proofDataArray[i].appCommitHash == keccak256(_appCircuitOutputs[i]), "invalid circuit output");
+        }
+
+        for (uint256 i=0;i<_appCircuitOutputs.length;i++) {
+            // one output has addr(20), [poolid(32), fromblk(8), toblk(8), uni amount(16)]
+            address sender = address(bytes20(_appCircuitOutputs[i][0:20]));
+            require(msg.sender==sender, "mismatch msg.sender and circuit output");
+            bytes calldata _appOutput = _appCircuitOutputs[i];
+            for (uint256 idx=20;idx<_appOutput.length;idx+=64) {
+                bytes32 poolid = bytes32(_appCircuitOutputs[i][20:52]);
+                if(poolId[poolid]) { // valid pool
+                    uint64 beginBlk = uint64(bytes8(_appCircuitOutputs[i][52:60]));
+                    uint64 endBlk = uint64(bytes8(_appCircuitOutputs[i][60:68]));
+                    if(beginBlk>lastBlockNum[sender][poolid]) {
+                        lastBlockNum[sender][poolid] = endBlk;
+                        amount += uint128(bytes16(_appCircuitOutputs[i][68:84]));
+                    }
                 }
             }
         }
         if(amount > 0 ) {
             uni.transfer(receiver, amount);
         }
-        // emit event?
     }
 }
